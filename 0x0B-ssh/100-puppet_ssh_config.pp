6fe75d3e7d8f76f21c38e3899d87efe126ee8621
Ibrahim Kyuka Avong <drkyavoni@live.com>
2025-06-27 20:56:46 +0100

    feat: update SSH connection script and add private/public key files

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,52 +1,13 @@
 # Using Puppet to make changes to our configuration file
 
-# exec {'delete_old_files':
-#   provider => 'shell',
-#   command  => '
-#     echo "Attempting to delete old files..." &&
-#     rm -vf /home/ubuntu/.ssh/school* &&
-#     echo "Deleting successful"
-#   ',
-#   onlyif   => 'test -f /home/ubuntu/.ssh/school',
-#   path     => ['/bin', '/usr/bin']
-# }
-
-# exec {'generate_ssh_key':
-#   provider => 'shell',
-#   command  => '
-#     echo "Attempting to create new keys" &&
-#     ssh-keygen -t rsa -f /home/ubuntu/.ssh/school -N "" &&
-#     echo "Creating new keys successful"
-#   ',
-#   path     => ['/bin', '/usr/bin']
-# }
-
-file {'/home/ubuntu/.ssh/config':
-  ensure  => file,
-  content => "
-    Host *
-      IdentityFile ~/.ssh/school
-      PasswordAuthentication no
-  "
+file_line { 'Turn off passwd auth':
+  path  => '/etc/ssh/ssh_config',
+  line  => '   PasswordAuthentication no',
+  match => '^[\s]*PasswordAuthentication',
 }
 
-# include stdlib
-
-# exec { 'backup_ssh_config':
-#   provider => 'shell',
-#   command  => 'cp /etc/ssh/ssh_config /etc/ssh/ssh_config.bak',
-#   unless   => 'test -f /etc/ssh/ssh_config.bak',
-#   path     => ['/bin', '/usr/bin'],
-# }
-
-# file_line {'No password':
-#   path  => '/etc/ssh/ssh_config',
-#   match => '^ PasswordAuthentication'
-#   line  => ' PasswordAuthentication no',
-# }
-
-# file_line { 'Set Identity File':
-#   path  => '/etc/ssh/ssh_config',
-#   match => '^ IdentityFile',
-#   line  => ' IdentityFile ~/.ssh/school',
-# }
+file_line { 'Declare identity file':
+  path  => '/etc/ssh/ssh_config',
+  line  => '   IdentityFile ~/.ssh/school',
+  match => '^[\s]*IdentityFile',
+}
