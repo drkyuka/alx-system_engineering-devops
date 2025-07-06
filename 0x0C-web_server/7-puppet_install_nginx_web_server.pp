e4ece9fbc8fdbb61de1febc2e43b60061d7c90d5
Ibrahim Kyuka Avong <drkyavoni@live.com>
2025-07-06 09:24:18 +0100

    fix: add puppet script for nginx installation and redirection setup

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,41 @@
+# puppet script 
+
+exec {'update all packages':
+  command => 'apt-get update && apt-get upgrade -y',
+}
+
+package { 'nginx':
+  ensure => installed,
+  require => Exec['update all packages'],
+}
+
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx'],
+}
+
+file {'/var/www/html/index.html':
+  ensure  => file,
+  content => 'Hello World!',
+  require => Package['nginx'],
+}
+
+file {'/etc/nginx/snippets/redirect_me.conf':
+  ensure  => file,
+  content => @("EOF")
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+    | EOF
+  require => Package['nginx'],
+}
+
+exec {'insert_nginx_redirect_snippet':
+  command => '/bin/sed -i "/^\\s*server\\s*{/r /etc/nginx/snippets/redirect_me.conf" /etc/nginx/sites-available/default',
+  require => [
+    File['/etc/nginx/snippets/redirect_me.conf'],
+    Package['nginx']
+    ],
+  notify => Service['nginx'], 
+}
